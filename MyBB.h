8d5dca89c9bc1b091d941fb4480a471e0e332c3a
afiqui <57716172+afiqui@users.noreply.github.com>
2020-03-26 21:05:29 -0300

    Add files via upload

---
+++ MyBB.h
@@ -0,0 +1,9 @@
+#ifndef MYBB_H_INCLUDED
+#define MYBB_H_INCLUDED
+
+void numtobin(char decimal);
+int decode();
+void regcode(char* reg);
+void opcode(char* teste[], int* type);
+
+#endif // MYBB_H_INCLUDED
